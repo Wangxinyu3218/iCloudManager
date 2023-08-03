f285e6e4c8415469b4fca45059e75a83d74c00af
星之卡鹿 <1037512136@qq.com>
2023-08-03 13:08:12 +0000

    [#]更新数据库密码

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',
-    password: '123456',
+    password: 'wtywfy5618754',
     database: 'bookkeeping_v4'
 });
 function query(sql) {
